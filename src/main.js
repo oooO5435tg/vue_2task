0c5434d7cab25103413e2f07bccdd255c3cbd741
oooO5435tg <morlean@inbox.ru>
2024-02-18 23:35:59 +0700

    добавление карточки - пока не работает

--- src/main.js
+++ src/main.js
@@ -1,3 +1,7 @@
+function getUniqueId(prefix) {
+    return `${prefix}-${Date.now()}-${Math.floor(Math.random() * 1000)}`;
+}
+
 new Vue({
     el: '#app',
     data(){
@@ -6,7 +10,7 @@ new Vue({
                 title: '',
             },
             newList: [
-                { title: '' }
+                { title: '', id: getUniqueId('newListItem') }
             ],
             plannedTasks: [],
             progressTasks: [],
@@ -17,6 +21,31 @@ new Vue({
         }
     },
     methods: {
+        addTask() {
+            const newListItems = this.newList.map((listItem, index) => ({
+                ...listItem,
+                id: getUniqueId(`listItem-${this.newList.length}`),
+                completed: false
+            }));
 
+            this.plannedTasks.push({
+                ...this.newTask,
+                lists: newListItems,
+                completedListItems: newListItems.map(item => item.completed), // Initialize completedListItems based on completed property
+                createdAt: new Date().toLocaleString(),
+                lastChange: null
+            });
+
+            this.newTask = {
+                title: '',
+                description: '',
+                deadline: '',
+                createdAt: '',
+                lastChange: null
+            };
+            this.newList = [
+                { title: '', id: getUniqueId('newListItem') }
+            ];
+        },
     }
 })
